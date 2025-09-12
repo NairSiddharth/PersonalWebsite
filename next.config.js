de1b297b144295149fa619739284e7b568970fb3
NairSiddharth <nair.sid01@gmail.com>
2025-09-12 16:28:07 -0500

    update the config to handle cache size issues

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: false // If you use <Image> component, needed for static export
+  },
+  // Disable caching for Cloudflare Pages build size limits
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.cache = false;
+    }
+    return config;
   }
 };
 
